1eec5ca94d340f1450c9e921a431ea0eee424577
The-Lule <oguzluleci@gmail.com>
2019-10-26 18:30:28 +0300

    Changing from Linux VM

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Oh hi! Just trying to change some source files. Hope this works."
+    return "Oh hi! Just trying to change some source files. Hope this works. By the way I cloned this repo to virtual machine linux so I also hope this works too."
 
 
 if __name__ == "__main__":
